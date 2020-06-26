62ec1c21199f8e13d0b35a15b971d1b18d387e5a
Joel Guerrero <joelg1014@hotmail.com>
2020-06-26 08:07:29 -0500

    19. Subject - Parte 2

--- curso-rxjs-inicio-master/src/index.ts
+++ curso-rxjs-inicio-master/src/index.ts
@@ -1,28 +1,38 @@
-import { Observable, Observer, Subject} from "rxjs";
+import { Observable, Observer, Subject } from "rxjs";
 
-const OBSERVER: Observer<any> ={
-    next : value => console.log('Next: ', value),
-    error:error => console.warn('error: ', error),
-    complete: () => console.info('completado')  
+const OBSERVER: Observer<any> = {
+    next: value => console.log('Next: ', value),
+    error: error => console.warn('error: ', error),
+    complete: () => console.info('completado')
 };
 
-const INTERVALO$ = new Observable<number>( subs => {
+const INTERVALO$ = new Observable<number>(subs => {
 
-  const INTERVALOID = setInterval(
-     () => subs.next( Math.random() ), 5000
-  );
-  return () => clearInterval( INTERVALOID);
-})
+    const INTERVALOID = setInterval(
+        () => subs.next(Math.random()), 1000
+    );
+    return () => {
+        clearInterval(INTERVALOID);
+        console.log('Intervalo destruido')
+    };
+});
 /**
  * 1- Casteo multiple
  * 2- Tambien es un observer
  * 3- Next, error y complete tambien se manejan
  */
 const SUBJECTS$ = new Subject();
-INTERVALO$.subscribe( SUBJECTS$ );
+const INTERVALSUBJECT = INTERVALO$.subscribe(SUBJECTS$);
 
-// const subs1 = INTERVALO$.subscribe(rnd => console.log('subs1', rnd));
+// const subs1 = INTERVALO$.subscribe(rnd => console.log);
 // const subs2 = INTERVALO$.subscribe(rnd => console.log('subs2', rnd));
 
-const subs1 = SUBJECTS$.subscribe(rnd => console.log('subs1', rnd));
-const subs2 = SUBJECTS$.subscribe(rnd => console.log('subs2', rnd));
+const subs1 = SUBJECTS$.subscribe(OBSERVER);
+const subs2 = SUBJECTS$.subscribe(OBSERVER);
+
+setTimeout(() => {
+    SUBJECTS$.next(10);
+    SUBJECTS$.complete();
+    INTERVALSUBJECT.unsubscribe();
+
+}, 3500);
