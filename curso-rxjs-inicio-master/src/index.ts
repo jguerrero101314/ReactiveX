cd83337957ed1ae4a40ac3e3b63665300a99f125
Joel Guerrero <joelg1014@hotmail.com>
2020-07-02 08:25:53 -0500

    67. Manejo de errores con el Fetch Api

---
+++ curso-rxjs-inicio-master/src/index.ts
@@ -0,0 +1,24 @@
+
+
+const url = 'https://api.github.com/users?per_page=5';
+const manejaErrores = ( response: Response) => {
+    if(!response.ok){
+        throw new Error ( response.statusText);
+    }
+    return response;
+}
+
+const fetchPromesa = fetch( url );
+/*
+fetchPromesa
+    .then( resp => resp.json())
+    .then( data => console.log( 'data:' ,data) )
+    .catch( err => console.warn('error en usuarios', err))
+*/
+
+fetchPromesa
+    .then( manejaErrores )
+    .then( resp => resp.json())
+    .then( data => console.log( 'data:' ,data) )
+    .catch( err => console.warn('error en usuarios', err))
+
